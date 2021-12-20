0133c7ca99af15db1c5031915db6dc80c3a9e026
Jair Araujo <jair.jay@hotmail.com>
2021-12-20 13:58:07 -0300

    Adding game styling and changing endgame styling

--- script.js
+++ script.js
@@ -49,7 +49,7 @@ function iniciarJogo(){
     for(i = 1; i < snake.length; i++){
         if(snake[0].x == snake[i].x && snake[0].y == snake[i].y){
             clearInterval(jogo);
-            alert("Game Over 😭");
+            document.getElementById("game-over").innerHTML = "Game Over!!!";
         }
     }
 
